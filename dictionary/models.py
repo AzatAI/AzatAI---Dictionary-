00a72fe4d1167ec070f3943090f42959ddac6bd5
azamat7087 <azaza7087@gmail.com>
2020-12-11 12:30:40 +0600

    Ru and EN search

--- dictionary/models.py
+++ dictionary/models.py
@@ -6,15 +6,15 @@ from users_app.views import set_id, get_hex_id
 class Word(models.Model):
     id = models.CharField(max_length=8, primary_key=True, unique=True, null=False)
     word_en = models.CharField(max_length=100)
-    # word_ru = models.CharField(max_length=30)
+    word_ru = models.CharField(max_length=30)
     text_en = models.TextField(max_length=30000)
-    # text_ru = models.CharField(max_length=3000)
+    text_ru = models.CharField(max_length=3000)
     audio_en = models.CharField(max_length=100)
-    # audio_ru = models.CharField(max_length=100)
+    audio_ru = models.CharField(max_length=100)
     wiki_en = models.CharField(max_length=100)
-    # wiki_ru = models.CharField(max_length=100)
+    wiki_ru = models.CharField(max_length=100)
     pronunciation_en = models.CharField(max_length=100)
-    # pronunciation_ru = models.CharField(max_length=100)
+    pronunciation_ru = models.CharField(max_length=100)
 
     def __str__(self):
         return self.word_en
--- dictionary/views.py
+++ dictionary/views.py
@@ -18,8 +18,32 @@ from bs4 import BeautifulSoup
 import wikipediaapi
 import re
 
+def translator(OBJ):
+    HEADERS = {
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
 
-def get_clear_text(items):
+    html = requests.get(f'https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=ru&dt=t&q={OBJ}',
+                        headers=HEADERS, params=None)
+    if html.status_code == 200:
+        arr = []
+        word = ''
+        for i in html.text:
+            arr.append(i)
+        for i1 in range(len(arr)):
+            if arr[i1] == '"':
+                arr = arr[i1:len(arr)]
+                i2 = 0
+                while not word.endswith('",'):
+                    word += arr[i2]
+                    i2 += 1
+                break
+        word = word[1:len(word) - 2]
+
+        return word
+
+
+def get_clear_text(items, lang):
     i = 0
     items_arr = []
     for i1 in items:
@@ -43,6 +67,10 @@ def get_clear_text(items):
     for i in items:
         arr.append(i)
     for i1 in range(len(arr)):
+        if lang == "ru":
+            if arr[i1] == "◆":
+                index = i1
+                break
         if arr[i1] == "\n":
             index = i1
             break
@@ -55,8 +83,8 @@ def get_html(url, HEADERS, params=None):
     r = requests.get(url, headers=HEADERS, params=params)
     return r
 
-def get_wikipedia_url(OBJ):
-    wiki_wiki = wikipediaapi.Wikipedia('en')
+def get_wikipedia_url(OBJ, lang):
+    wiki_wiki = wikipediaapi.Wikipedia(lang)
     page_py = wiki_wiki.page(OBJ)
 
     if page_py.exists():
@@ -65,10 +93,10 @@ def get_wikipedia_url(OBJ):
         return False
 
 
-def get_content(html, OBJ, HEADER, HOST, URL):
+def get_content(html, OBJ, HEADER, HOST, URL, lang):
     soup = BeautifulSoup(html, 'html.parser')
     items = soup.ol.get_text()
-    meaning = get_clear_text(items)
+    meaning = get_clear_text(items, lang)
     audio = soup.find('td', class_='audiometa')
     pronunciation = soup.find('span', class_="IPA").get_text()
     link = str(audio).split('href="')[1].split('"')[0]
@@ -81,7 +109,7 @@ def get_content(html, OBJ, HEADER, HOST, URL):
         'meaning': meaning,
         'link_audio': link_audio,
         'url': URL,
-        'wikipedia_url': get_wikipedia_url(OBJ),
+        'wikipedia_url': get_wikipedia_url(OBJ, lang),
         'pronunciation': pronunciation,
 
     }
@@ -98,14 +126,30 @@ def get_header(request):
     agent_accept.append(head['ACCEPT'])
     return agent_accept
 
+
+def get_params(word, lang):
+    URL = f'https://{lang}.wiktionary.org/wiki/{word}'
+
+    HOST = f'https://{lang}.wiktionary.org/'
+
+    return [URL, HOST]
+
+
+
 def get_word(request, word, lang):
     agent_accept = get_header(request)
-    URL = f'https://en.wiktionary.org/wiki/{word}'
+    if lang == "ru":
+        word = translator(word)
+    # URL = f'https://en.wiktionary.org/wiki/{word}'
     HEADERS = {'user-agent': agent_accept[0], 'accept': agent_accept[1]}
-    HOST = 'https://en.wiktionary.org/'
+    # HOST = 'https://en.wiktionary.org/'
+    params = get_params(word, lang)
+    URL = params[0]
+    HOST = params[1]
+
     html = get_html(URL, HEADERS, None)
     if html.status_code == 200:
-        object = get_content(html.text, word, HEADERS, HOST, URL)
+        object = get_content(html.text, word, HEADERS, HOST, URL, lang)
         return object
     else:
         return 'Not found'
@@ -115,6 +159,7 @@ class Main(View):
 
     def post(self, request):
         word = request.POST.get('word')
+        word_ru = translator(word)
         words = Word.objects.all()
         en_words = []
         for i in words:
@@ -123,8 +168,19 @@ class Main(View):
             return redirect('dict_main_url')
         else:
             en_word = get_word(request, word, 'en')
-            # ru_word = get_word(request, word, 'ru')
-            Word.objects.create(word_en=word, text_en=en_word['meaning'], audio_en=en_word['link_audio'], wiki_en=en_word['wikipedia_url'], pronunciation_en=en_word['pronunciation'])
+            ru_word = get_word(request, word, 'ru')
+            Word.objects.create(word_en=word,
+                                text_en=en_word['meaning'],
+                                audio_en=en_word['link_audio'],
+                                wiki_en=en_word['wikipedia_url'],
+                                pronunciation_en=en_word['pronunciation'],
+                                word_ru=word_ru,
+                                text_ru=ru_word['meaning'],
+                                audio_ru=ru_word['link_audio'],
+                                wiki_ru=ru_word['wikipedia_url'],
+                                pronunciation_ru=ru_word['pronunciation'],
+
+                                )
         return redirect('dict_main_url')
 
 
